7191c144730495375ae9cff443835a8985edd400
Guras-Hub <167527683+Guras-Hub@users.noreply.github.com>
2024-06-01 16:31:33 +0200

    Add files via upload

--- BRIDGE.py
+++ BRIDGE.py
@@ -9,12 +9,12 @@ def main():
     col1, col2 = st.columns(2)
     
     with col1:
-        if st.button('Vai a Enchanted Equities v2', help='Clicca per visitare la versione v2 del sito'):
+        if st.button('Equities v2', help='Clicca per visitare la versione v2 del sito'):
             st.success('Clicca e sarai reindirizzato a Equities v2.')
             st.markdown('### [Visita Enchanted Equities v2](https://enchanted-equities-v2.streamlit.app)', unsafe_allow_html=True)
     
     with col2:
-        if st.button('Vai a Enchanted Equities Test Analyze Pro', help='Clicca per visitare la versione Test Analyze Pro del sito'):
+        if st.button('Test Analyze Pro', help='Clicca per visitare la versione Test Analyze Pro del sito'):
             st.success('Clicca e sarai reindirizzato a Test Analyze Pro.')
             st.markdown('### [Visita Enchanted Equities Test Analyze Pro](https://enchanted-equities-test-analyze-pro.streamlit.app)', unsafe_allow_html=True)
 
